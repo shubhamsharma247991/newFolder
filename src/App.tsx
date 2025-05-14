fc87c3b7583fb8ecd87206cc2ef498f0a4ae2256
shubham_247_991 <141959332+shubham497sharma@users.noreply.github.com>
2025-05-14 22:29:52 +0530

    Final Fix

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,8 @@ function App() {
 
         {/* Authentication layout */}
         <Route element={<AuthenticationLayout/>}>
-          <Route path="signin" element={<SignInPage />}/>
-          <Route path="signup" element={<SignUpPage />}/>
+          <Route path="signin/*" element={<SignInPage />}/>
+          <Route path="signup/*" element={<SignUpPage />}/>
         </Route>
 
         {/* protected routes */}
