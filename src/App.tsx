123e52834316780b925740bbec4886c18c482a40
shubham_247_991 <141959332+shubham497sharma@users.noreply.github.com>
2025-05-14 22:55:39 +0530

    new3

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         {/* Authentication layout */}
         <Route element={<AuthenticationLayout/>}>
           <Route path="/signin/*" element={<SignInPage />}/>
-          <Route path="signup/*" element={<SignUpPage />}/>
+          <Route path="/signup/*" element={<SignUpPage />}/>
         </Route>
 
         {/* protected routes */}
